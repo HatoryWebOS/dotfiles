fe2c02613257dc4f3bb150f20026253d6ca01a89
HatoryWebOS <70821647+HatoryWebOS@users.noreply.github.com>
2020-09-06 17:37:11 +0200

    Create bootstrap.sh

---
+++ voidlinux/bootstrap.sh
@@ -0,0 +1,7 @@
+### General voidrice post-installation script ###
+
+cat ~/.zshrc | sed "s/nik123true/void/g" > ~/.bashrc
+curl https://raw.githubusercontent.com/HatoryWebOS/HatoryHost/master/bashrc >> ~/.zshrc
+sudo xbps-install alsa-plugins-pulseaudio cmus-pulseaudio cmus moc wget curl
+feh --bg-scale ../wall/main.png
+echo "Finished. Close this terminal and open a new one".
