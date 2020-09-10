4f622e781be0c281764d3016aacfd3d1e869c711
HatoryInWeb <70821647+HatoryWebOS@users.noreply.github.com>
2020-09-10 02:40:11 +0200

    Update bootstrap.sh

--- voidlinux/bootstrap.sh
+++ voidlinux/bootstrap.sh
@@ -1,7 +1,7 @@
 ### General voidrice post-installation script ###
 
-cat ~/gh/dotfiles/.zshrc | sed "s/nik123true/void/g" > ~/gh/dotfiles/.bashrc
-curl https://raw.githubusercontent.com/HatoryWebOS/dotfiles/master/bashrc >> ~/.zshrc
+cat ~/.zshrc | sed "s/nik123true/void/g" > ~/.bashrc
+curl https://raw.githubusercontent.com/KonradIT/dotfiles/master/bashrc >> ~/.zshrc
 sudo xbps-install alsa-plugins-pulseaudio cmus-pulseaudio cmus moc wget curl
 feh --bg-scale ../wall/main.png
 echo "Finished. Close this terminal and open a new one".
