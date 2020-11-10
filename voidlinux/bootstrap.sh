8e9c7de01554925e9ba4e3207c8c2857521ea6f4
HatoryInWeb <bartekszewczyk01@gmail.com>
2020-11-10 07:19:43 +0100

    Delete bootstrap.sh

--- voidlinux/bootstrap.sh
+++
@@ -1,7 +0,0 @@
-### General voidrice post-installation script ###
-
-cat ~/.zshrc | sed "s/nik123true/void/g" > ~/.bashrc
-curl https://raw.githubusercontent.com/KonradIT/dotfiles/master/bashrc >> ~/.zshrc
-sudo xbps-install alsa-plugins-pulseaudio cmus-pulseaudio cmus moc wget curl
-feh --bg-scale ../wall/main.png
-echo "Finished. Close this terminal and open a new one".
